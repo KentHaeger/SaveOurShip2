b529c3b44c6a9cfe027ab0b407de5b156a1cce98
user <fldpi@tutanota.com>
2024-08-24 17:54:18 +0300

    Electrice engine support - works, but red errors on load due to  multiple power comps on said engine.

---
+++ Source/1.5/Comp/CompRefuelableElectric.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace SaveOurShip2
+{
+	class CompRefuelableElectric : CompRefuelable
+	{
+		public CompPowerBattery battery;
+		public override void Initialize(CompProperties props)
+		{
+			base.Initialize(props);
+		}
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			battery = parent.GetComp<CompPowerBattery>();
+		}
+		public override void CompTick()
+		{
+			base.CompTick();
+			if (battery == null)
+			{
+				return;
+			}
+			if (Find.TickManager.TicksGame % 30 == 0)
+			{
+				fuel = battery.StoredEnergy;
+			}
+		}
+	}
+}
