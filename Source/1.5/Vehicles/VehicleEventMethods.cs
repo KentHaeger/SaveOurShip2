aba6966d5fdb125c815c4d2a7e9668cf540c0505
user <fldpi@tutanota.com>
2025-06-18 19:07:04 +0300

    As event method called during landing sequence, switched unfog to post spawn setup instead.

--- Source/1.5/Vehicles/VehicleEventMethods.cs
+++ Source/1.5/Vehicles/VehicleEventMethods.cs
@@ -25,24 +25,9 @@ namespace SaveOurShip2
 
         public static void ShuttleLanded(DefaultTakeoff landing)
         {
-            VehiclePawn vehicle = landing.vehicle; 
-            CompVehicleHeatNet net = vehicle.TryGetComp<CompVehicleHeatNet>();
+            CompVehicleHeatNet net = landing.vehicle.TryGetComp<CompVehicleHeatNet>();
             landing.vehicle.ignition.Drafted = false;
             net?.RebuildHeatNet();
-            if (vehicle.Faction == Faction.OfPlayer && vehicle.Spawned)
-            {
-                // Failsafe - unfog from vehicle location.
-                FloodFillerFog.FloodUnfog(vehicle.Position, vehicle.Map);
-                // When landing on ship bay that is already unfogged (bay feature) within fogged room need to unfog adjacent.
-                Building bay = (Building)vehicle.Position.GetThingList(vehicle.Map).Where(t => ((t as ThingWithComps)?.TryGetComp<CompShipBay>() ?? null) != null).DefaultIfEmpty(null).First();
-                if (bay != null)
-                {
-                    foreach (IntVec3 cellToUnfog in GenAdj.CellsAdjacentCardinal(bay).Where(cell => !cell.Impassable(vehicle.Map)))
-                    {
-                        FloodFillerFog.FloodUnfog(cellToUnfog, vehicle.Map);
-                    }
-                }
-            }
         }
     }
 }
