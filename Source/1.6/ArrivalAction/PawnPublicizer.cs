711f1f00003cc348d4bbf022e93b9fa7c73e9349
user <fldpi@tutanota.com>
2025-07-24 13:01:46 +0300

    Fix archotech spore install consciusness error

--- Source/1.6/ArrivalAction/PawnPublicizer.cs
+++ Source/1.6/ArrivalAction/PawnPublicizer.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Reflection;
 using RimWorld;
 using UnityEngine;
 using Vehicles;
 using Verse;
+using HarmonyLib;
 
 namespace SaveOurShip2
 {
-	public class PawnPublicizer : Pawn
+	public class PawnPublicizer
 	{
 		// Makes tick, which was hidden in 1.6 update available again for mod purposes
 		public static void DoTick(Pawn pawn)
 		{
-			((PawnPublicizer)pawn).Tick();
+			MethodInfo tickMethod = typeof(Pawn).GetMethod("Tick", BindingFlags.Instance | BindingFlags.NonPublic);
+			tickMethod.Invoke(pawn, new object[] { });
 		}
 	}
 }
