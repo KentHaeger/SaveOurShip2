bbfef34346f8e24c521e4c79c5fdecbf59e4e8e7
user <fldpi@tutanota.com>
2026-05-12 12:11:40 +0300

    Added decouplers: automatically exploding on tick comp. Intended to build fighter/ship wings that are connected and recognized as one ship, but disconnected on tick via explosion. Upgrading to disconnect on ship spawn is possible.

---
+++ Source/1.6/Comp/CompExplosiveInstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RimWorld;
+
+namespace SaveOurShip2
+{
+	class CompExplosiveInstant : CompExplosive
+	{
+		public override void CompTick()
+		{
+			if (parent.Spawned)
+			{
+				Detonate(parent.MapHeld);
+			}
+		}
+	}
+}
