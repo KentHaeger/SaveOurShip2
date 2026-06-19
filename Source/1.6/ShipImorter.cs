698572e3670ad338d671f790a528ed1fa7c7d4d8
user <fldpi@tutanota.com>
2026-06-19 13:25:48 +0300

    Saving colored ships implementation that tries to add colorDefs dynamically. It work very well in-game, but breaks ovver save-load. It is beyound base game limitaions to make that save-lading work reliable due to short hashes used for color.
    The implementation have to be changed to use a palette of color defs
    that will always add the same colors to def database.
    
    With 32 (great looking) to 16 (decently looking) colors for 1 gradient
    transition, at least a limited variant of that feature is doable.

--- Source/1.6/ShipImorter.cs
+++ Source/1.6/ShipImorter.cs
@@ -13,6 +13,7 @@ namespace SaveOurShip2
 	{
 		private static HashSet<IntVec3> cellsToDesignate;
 		private static HashSet<IntVec3> floorCells;
+		const string noMapMessage = "To import ship blueprint, switch from world view to loacl map view";
 
 		private static bool IsRed(Color color)
 		{
@@ -97,38 +98,51 @@ namespace SaveOurShip2
 			return origin + finalRot.AsIntVec3 * (Mathf.Max(0, length - 1) / 2) + offset;
 		}
 		// Naname is actual mod name, means diagonal in Japanese 
+		private static Texture2D LoadTextureFromSos2Folder(string fileName)
+        {
+			string path = Path.Combine(GenFilePaths.SaveDataFolderPath, "SoS2", fileName);
+			if (!File.Exists(path))
+			{
+				Messages.Message("Blueprint file doesn't exist", null, MessageTypeDefOf.NeutralEvent);
+				return null;
+			}
+			byte[] fileData = File.ReadAllBytes(path);
+			Texture2D tex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+			tex.filterMode = FilterMode.Bilinear;
+			tex.wrapMode = TextureWrapMode.Clamp;
+			if (!tex.LoadImage(fileData))
+			{
+				tex = new Texture2D(2, 2, TextureFormat.RGB24, false);
+				tex.filterMode = FilterMode.Bilinear;
+				tex.wrapMode = TextureWrapMode.Clamp;
+				if (!tex.LoadImage(fileData))
+				{
+					Messages.Message("Error loading blueprint image", null, MessageTypeDefOf.NeutralEvent);
+					return null;
+				}
+			}
+			return tex;
+		}
+
 		public static void ImportShipDesign(bool placeActualBuildings, bool useNanameWalls = false)
 		{
 			Map map = Find.CurrentMap;
 			if (map == null)
 			{
-				Messages.Message("To import ship blueprint, switch from world view to loacl map view", null, MessageTypeDefOf.NeutralEvent);
+				Messages.Message(noMapMessage, null, MessageTypeDefOf.NeutralEvent);
 				return;
 			}
-			string path = Path.Combine(GenFilePaths.SaveDataFolderPath, "SoS2\\Blueprint.png");
-			if (!File.Exists(path))
-            {
-				Messages.Message("Blueprint file doesn't exist", null, MessageTypeDefOf.NeutralEvent);
-				return;
-			}
-			byte[] fileData = File.ReadAllBytes(path);
-			Texture2D tex = new Texture2D(2, 2, TextureFormat.RGBA32, false);
-			if (!tex.LoadImage(fileData))
-            {
-				tex = new Texture2D(2, 2, TextureFormat.RGB24, false);
-				if (!tex.LoadImage(fileData))
-				{
-					Messages.Message("Error loading blueprint image", null, MessageTypeDefOf.NeutralEvent);
-					return;
-				}
-			}
+
 			if (useNanameWalls && DefDatabase<ThingDef>.GetNamedSilentFail("Ship_Beam_NAWDiagonal") == null)
             {
 				Messages.Message("Naname ship hull def not found", null, MessageTypeDefOf.NeutralEvent);
 				return;
 			}
-			tex.filterMode = FilterMode.Bilinear;
-			tex.wrapMode = TextureWrapMode.Clamp;
+			Texture2D tex = LoadTextureFromSos2Folder("Blueprint.png");
+			if (tex == null)
+            {
+				return;
+            }
 			cellsToDesignate = new HashSet<IntVec3>();
 			floorCells = new HashSet<IntVec3>();
 
@@ -238,6 +252,127 @@ namespace SaveOurShip2
 				Designator_Plan_Add adder = new Designator_Plan_Add();
 				adder.PlanCells(cellsToDesignate);
 			}
+		}
+
+		public static void ImportFloorAndColor(Map map)
+        {
+			if (map == null)
+			{
+				Messages.Message(noMapMessage, null, MessageTypeDefOf.NeutralEvent);
+				return;
+			}
+			Texture2D texFloor = LoadTextureFromSos2Folder("ColorFloor.png");
+			Texture2D texWall = LoadTextureFromSos2Folder("ColorWall.png");
+			if (texFloor == null || texWall == null)
+			{
+				return;
+			}
+
+
+			int processedBuildings = 0;
+			// Dictionary<Color, ColorDef> colorDefs = new Dictionary<Color, ColorDef>();
+			for (int x = 0; x < Mathf.Min(map.Size.x, texFloor.width, texWall.width); x++)
+			{
+				for (int z = 0; z < Mathf.Min(map.Size.z, texFloor.height, texFloor.height); z++)
+				{
+					Color floorColor = texFloor.GetPixel(x, z); 
+					Color wallColor = texWall.GetPixel(x, z);
+					IntVec3 tile = new IntVec3(x, 0, z);
+					bool hasPlating;
+					bool hasBay;
+					PlaceWorker_OnShipHull.HasPlatingAndRestrictedBayFor(null, tile, map, out hasPlating, out hasBay);
+					if (hasPlating)
+					{
+						map.terrainGrid.SetTerrain(tile, TerrainDefOf.MetalTile);
+						ColorDef colorDef = MakeAndRegisterColorDef(floorColor);
+						ColorDef databaseColor = DefDatabase<ColorDef>.GetNamedSilentFail(colorDef.defName);
+						if (databaseColor == null)
+                        {
+							DefDatabase<ColorDef>.Add(colorDef);
+							databaseColor = DefDatabase<ColorDef>.GetNamed(colorDef.defName);
+						}
+						/*if(!colorDefs.ContainsKey(color))
+                        {
+							ColorDef colorDef = MakeColorDef(color);
+							DefDatabase<ColorDef>.Add(colorDef);
+							colorDefs.Add(color, colorDef);
+						}*/
+						// map.terrainGrid.SetTerrainColor(tile, colorDefs[color]);
+						map.terrainGrid.SetTerrainColor(tile, databaseColor);
+					}
+					foreach (Thing t in tile.GetThingList(map))
+					{
+						if (t is Building b)
+						{
+							if (ResourceBank.CornerDefs.Contains(b.def) || ResourceBank.HullDefs.Contains(b.def))
+                            {
+								processedBuildings++;
+								ColorDef colorDef = MakeAndRegisterColorDef(wallColor);
+								ColorDef databaseColor = DefDatabase<ColorDef>.GetNamedSilentFail(colorDef.defName);
+								if (databaseColor == null)
+								{
+									DefDatabase<ColorDef>.Add(colorDef);
+									databaseColor = DefDatabase<ColorDef>.GetNamed(colorDef.defName);
+								}
+								//b.paintColorDef = databaseColor;
+								b.ChangePaint(databaseColor);
+							}
+						}
+					}
+				}
+			}
+			Log.Message($"SoS 2: Processed buildings for coloring: {processedBuildings}");
+			map.mapDrawer.WholeMapChanged(MapMeshFlagDefOf.Buildings);
+			map.mapDrawer.WholeMapChanged(MapMeshFlagDefOf.Things);
+			map.mapDrawer.RegenerateEverythingNow();
+			
+		}
+
+		private static float LimitcolorComponentBits(float component)
+		{
+			// To avoid excess resource use due to graphic data stored for one building too many times for different colors,
+			// limit color precision so that long rgadient transitions don't spawn like 100+ color defs.
+			// Bumber of bits that can be stripped is verified by visual review.
+			byte byteColor = (byte)(component * byte.MaxValue);
+			byteColor = (byte)(byteColor & ~0b111);
+			return (float)byteColor / byte.MaxValue;
+		}
+		private static Color LimitColorBits(Color sourceColor)
+		{
+			return new Color(
+				LimitcolorComponentBits(sourceColor.r),
+				LimitcolorComponentBits(sourceColor.g),
+				LimitcolorComponentBits(sourceColor.b),
+				LimitcolorComponentBits(sourceColor.a));
+		}
+
+		public static ColorDef MakeAndRegisterColorDef(Color color)
+		{
+			if (ShipInteriorMod2.WorldComp.ColorCapReached())
+			{
+				return null;
+			}
+			ColorDef colorDef = MakeColorDef(color);
+			ColorDef databaseColor = DefDatabase<ColorDef>.GetNamedSilentFail(colorDef.defName);
+			if (databaseColor == null)
+			{
+				ShipInteriorMod2.WorldComp.AddColor(color); 
+				DefDatabase<ColorDef>.Add(colorDef);
+				databaseColor = DefDatabase<ColorDef>.GetNamed(colorDef.defName);				
+			}
+			return databaseColor;
+		}
+
+		public static ColorDef MakeColorDef(Color color)
+        {
+			color = LimitColorBits(color);
+			ColorDef result = new ColorDef();
+			result.randomlyPickable = false;
+			result.color = color;
+			result.defName = "Color" + color.r.ToString("F2") + color.g.ToString("F2") + color.b.ToString("F2");
+			result.label = result.defName;
+			ShortHashGiver.GiveShortHash(result, typeof(ColorDef), ShortHashGiver.takenHashesPerDeftype[typeof(ColorDef)]);
+			return result;
 		}
 	}
 }
